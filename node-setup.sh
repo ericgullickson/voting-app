368e24c202eff043aa53a1fe3be49dd6631920a3
Eric Gullickson <16152721+ericgullickson@users.noreply.github.com>
2024-07-25 10:51:23 -0500

    update script, add cloud-init

--- node-setup.sh
+++ node-setup.sh
@@ -12,17 +12,19 @@ VALID_WORDS=("db" "worker" "vote" "result" "redis")
 
 # Check if the provided parameter is in the list of valid words
 if [[ " ${VALID_WORDS[@]} " =~ " $1 " ]]; then
+  #Change hostname
+  sudo hostnamectl set-hostname $1
   #Add Docker Repo
-  dnf -y config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+  sudo dnf -y config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
   #
   #Install docker 
-  dnf -y install docker-ce docker-ce-cli containerd.io
+  sudo dnf -y install docker-ce docker-ce-cli containerd.io
   #
   #Start Docker service
-  systemctl start docker
+  sudo systemctl start docker
   #
   #Start the container depending on parameter
-  docker compose --file ./docker-compose.$1.yml up -d
+  sudo docker compose --file ./docker-compose.$1.yml up -d
 
 else
   echo "Invalid input. Please provide one of the following: db, worker, vote, result, redis"
